2928e26aa0ebbf1576555f1f6646304ba6c20141
Lucas Vieira <vieira.lucasmm@gmail.com>
2022-10-08 14:04:13 -0300

    Add interaction with the Greeting contract

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import './App.css';
 
+const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
 function App() {
+  const [greeting, setGreetingValue] = useState('');
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async function feetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        const data = await contract.greet();
+        console.log('data:', data);
+      } catch (err) {
+        console.error('Error:', err);
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue('');
+      await transaction.wait();
+      feetchGreeting();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <button onClick={feetchGreeting}>Feetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input
+          name='greetingValue'
+          value={greeting}
+          onChange={(e) => setGreetingValue(e.target.value)}
+          placeholder='Set greeting'
+        />
       </header>
     </div>
   );
